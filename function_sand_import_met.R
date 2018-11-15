c6b344807cd1f1843900dc598b0bde2f786d7a57
Jackie Ouzman <jackie.ouzman@csiro.au>
2018-11-15 20:15:32 +1030

    function to download SILO data

---
+++ function_sand_import_met.R
@@ -0,0 +1,55 @@
+
+#install.packages("RCurl")
+#install.packages("XML")
+library(XML)
+library(RCurl)
+
+library(readr)
+#dataset <- read_csv(NULL)
+#View(dataset)
+
+getSiloMet_jax <- function(name = NULL) {
+  stationID <- ifelse(name == 'Waikerie', 24018,
+                ifelse(name == 'Carwarp', 76005,
+                ifelse(name == 'Ouyen', 76047,
+                ifelse(name == 'Karoonda', 25006,
+                ifelse(name == 'Murlong', 18046,
+                ifelse(name == 'Yenda', 75079,
+                ifelse(name == 'Lameroo', 25509,
+                ifelse(name == 'Bute', 21012,
+                ifelse(name == 'Brimpton Lake', 18005,
+                ifelse(name == 'Cadgee', 26099))))))))))
+  
+  #check my apiKey it is Kvsj6LwGthiRRUoxGazEDLikHvDxh5kOJDvbRZp4
+  startDate <- paste('19600101',sep='')
+  finishDate <- paste('20171231',sep='')
+  
+  
+  siloUrl <- 'https://siloapi.longpaddock.qld.gov.au/pointdata'
+  siloUrl <- paste(siloUrl, '?apikey=','Kvsj6LwGthiRRUoxGazEDLikHvDxh5kOJDvbRZp4', sep='')
+  siloUrl <- paste(siloUrl,'&station=',sprintf('%05d', stationID), sep='')
+  siloUrl <- paste(siloUrl,'&start=', startDate, sep='')
+  siloUrl <- paste(siloUrl,'&finish=', finishDate, sep='')
+  siloUrl <- paste(siloUrl,'&format=CSV', sep='')
+  siloUrl <- paste(siloUrl,'&variables=daily_rain,max_temp,min_temp', sep='')
+  
+  print(siloUrl)
+  
+  #dataUrl <- getURL(siloUrl)
+  
+  #df <- read.csv(textConnection(dataUrl),stringsAsFactors=FALSE)
+  #print(dataUrl)
+  #return(csvDat)
+  #df <- readHTMLTable(dataUrl, stringsAsFactors = FALSE)
+  #again1 <- read_csv("https://siloapi.longpaddock.qld.gov.au/pointdata?apikey=Kvsj6LwGthiRRUoxGazEDLikHvDxh5kOJDvbRZp4&station=18005&start=19600101&finish=20171231&format=CSV&variables=daily_rain,max_temp,min_temp")
+  again <- read_csv(siloUrl)
+  return(again)
+}
+
+
+
+met_file <- getSiloMet_jax("Brimpton Lake")
+
+
+
+
